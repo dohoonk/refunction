468e7e06c421f0db466a2103055c6401838a652c
Dohoon Kim <dohoonk.smg@gmail.com>
2018-09-19 13:25:28 -0700

    add carrierwave

--- Gemfile
+++ Gemfile
@@ -36,8 +36,8 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'bootstrap-sass', '~> 3.3.7'
 gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
-# gem 'carrierwave'
-# gem 'mini_magick'
+gem 'carrierwave'
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180918210511) do
+ActiveRecord::Schema.define(version: 20180919184618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180918210511) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "services", force: :cascade do |t|
