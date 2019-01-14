b9da82bb729596c52bb3f2da0369a75d76768c8d
Dohoon Kim <dohoonk.smg@gmail.com>
2019-01-14 00:39:09 -0800

    random team on front

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @published_testimonials = AdminTestimonial.where(published: true).sample(3)
     @admin_enquiry = AdminEnquiry.new
+    @featured_teams = AdminTeam.order("RANDOM()").limit(4)
   end
 end
