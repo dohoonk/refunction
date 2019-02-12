e75eaa90f17494a68904850523f3db4e7e8d546b
Dohoon Kim <dohoonk.smg@gmail.com>
2019-02-11 19:41:23 -0800

    changes

--- app/controllers/our_teams_controller.rb
+++ app/controllers/our_teams_controller.rb
@@ -2,6 +2,7 @@ class OurTeamsController < ApplicationController
   def index
     @search = AdminTeam.search(params[:q])
     @teams = @search.result
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 
   def show
@@ -10,30 +11,36 @@ class OurTeamsController < ApplicationController
   def office_administration
       @search = AdminTeam.search(params[:q])
     @teams = AdminTeam.where(job_category_id: 1)
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 
   def occupational_therapists
       @search = AdminTeam.search(params[:q])
     @teams = AdminTeam.where(job_category_id: 2)
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 
   def physiotherapists
       @search = AdminTeam.search(params[:q])
     @teams = AdminTeam.where(job_category_id: 3)
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 
   def kinesiologist
       @search = AdminTeam.search(params[:q])
     @teams = AdminTeam.where(job_category_id: 4)
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 
   def psychology
       @search = AdminTeam.search(params[:q])
     @teams = AdminTeam.where(job_category_id: 5)
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 
   def counsellors
       @search = AdminTeam.search(params[:q])
     @teams = AdminTeam.where(job_category_id: 6)
+    @teams = @teams.sort_by {|x| x.first_name}
   end
 end
