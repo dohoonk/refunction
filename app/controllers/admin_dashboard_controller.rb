2a07729c7a58182ce19bfeac9ef9f6f373a03c78
Dohoon Kim <dohoonk.smg@gmail.com>
2019-03-04 11:27:56 -0800

    changes

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,7 +1,6 @@
 class AdminDashboardController < ApplicationController
   before_action :logged_in_check
   def index
-    binding.pry
     @visits = Ahoy::Visit.where(started_at: Date.today - 7...Date.today + 2)
     @events = Ahoy::Event.where(time: Date.today - 7...Date.today + 2)
     @visits_last_ten = Ahoy::Visit.last(10)
@@ -9,6 +8,12 @@ class AdminDashboardController < ApplicationController
     Ahoy::Event.all.each do |f|
       @array << f.properties["controller"]
     end
+
+    @visits_today = Ahoy::Visit.where(started_at: Date.today...Date.today + 2).count
+
+    @visits_this_week = Ahoy::Visit.where(started_at: Date.today - 7...Date.today + 2).count
+
+    @visits_this_month = Ahoy::Visit.where(started_at: Date.today - 32...Date.today + 2).count
 
     @b = Hash.new(0)
 
