3ec2e36099a1492185c81d026c25a6ccab012bad
Dohoon Kim <dohoonk.smg@gmail.com>
2019-04-30 21:09:25 -0700

    changes

--- app/mailers/referral_mailer.rb
+++ app/mailers/referral_mailer.rb
@@ -3,7 +3,7 @@ default :from => 'info@refunction.ca'
 # send a signup email to the user, pass in the user object that   contains the user's email address
 def send_signup_email(referral)
   @referral = referral
-  mail( :to => 'tonykim.tech@gmail.com',
+  mail( :to => 'info@refunction.ca',
   :subject => 'Referral Received' )
 end
 
