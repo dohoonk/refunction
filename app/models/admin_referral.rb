93090af4e6f7a0726191db4f19c64ff67f4f7bbf
Dohoon Kim <dohoonk.smg@gmail.com>
2019-06-12 10:49:09 -0700

    remove the requirements

--- app/models/admin_referral.rb
+++ app/models/admin_referral.rb
@@ -1,9 +1,9 @@
 class AdminReferral < ApplicationRecord
-  validates :client_name, presence: true
-  validates :name, presence: true
-  validates :company, presence: true
-  validates :phone, presence: true
-  validates :email, presence: true
-  validates :client_phone, presence: true
-  validates :advertisement, presence: true
+  # validates :client_name, presence: true
+  # validates :name, presence: true
+  # validates :company, presence: true
+  # validates :phone, presence: true
+  # validates :email, presence: true
+  # validates :client_phone, presence: true
+  # validates :advertisement, presence: true
 end
