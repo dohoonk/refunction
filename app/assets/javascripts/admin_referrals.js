abe5a6081e4d4f2895b419834f392633754b1abe
Ediyu <peterhjhan@gmail.com>
2020-01-07 14:20:25 -0800

    wording changes

--- app/assets/javascripts/admin_referrals.js
+++ app/assets/javascripts/admin_referrals.js
@@ -1,7 +1,13 @@
 $( document ).on('turbolinks:load', function() {
 
+  displayInterpreterLanguageOnYes();
 
-  //Shows the input field for Referral Forms when Select is used on the Interpreter Required?
+  displayInterpreterLanguageOnViewOfReferral();
+
+});
+
+//Shows the input field for Referral Forms when Select is used on the Interpreter Required?
+function displayInterpreterLanguageOnYes() {
   $( '.interpreter-required').on('change', 'select', function() {
     if ( ($(this).val() == 'Yes') ) {
       $('.interpreter-language').children().removeClass('d-none');
@@ -9,14 +15,13 @@ $( document ).on('turbolinks:load', function() {
       $('.interpreter-language').children().addClass('d-none');
     }
   });
-
-
-  //on the form's show page, shows the interepreter's language if the select is Yes
+}
+//on the form's show page, shows the interepreter's language if the select is Yes
+function displayInterpreterLanguageOnViewOfReferral() {
   if( $('#showReferral').length != 0) {
     let languageNeeded = $( '#showReferral' ).find('.interpreter-required-result div');
     if($(languageNeeded).html().includes('Yes')) {
       $( '#showReferral').find('.interpreter-required-language div').removeClass('d-none');
     }
   }
-
-});
+}
