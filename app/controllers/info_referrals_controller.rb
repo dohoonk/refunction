a5dd5205d0225e4b76f760fa44f7c289d2537342
Ediyu <peterhjhan@gmail.com>
2020-01-07 10:54:28 -0800

    refactor mailer code

--- app/controllers/info_referrals_controller.rb
+++ app/controllers/info_referrals_controller.rb
@@ -1,6 +1,5 @@
 class InfoReferralsController < ApplicationController
     def show
-
         @referral = AdminReferral.find_param(params[:id])
     end
 end
--- app/mailers/referral_mailer.rb
+++ app/mailers/referral_mailer.rb
@@ -6,16 +6,7 @@ class ReferralMailer < ApplicationMailer
     # mail( :to => 'info@refunction.ca,referrals@refunction.ca,referral@refunction.ca',
     # :subject => 'Referral Received' )
     subject = "Referral Received"
-
-    if @referral.referral_form_type == 'medlegal'
-      subject += " Claim #: #{@referral.client_file_number}" if @referral.client_file_number.present?
-
-      subject += " File #: #{@referral.client_sub_file_number}" if @referral.client_sub_file_number.present?
-    elsif @referral.referral_form_type == 'employer'
-      subject += " Policy #: #{@referral.client_file_number}" if @referral.client_file_number.present?
-
-      subject += " Client ID #: #{@referral.client_sub_file_number}" if @referral.client_sub_file_number.present?
-    end
+    subject += add_email_header(@referral)
 
     mail( :to => 'peter.h.9002@gmail.com',
     :subject => subject )
@@ -24,20 +15,31 @@ class ReferralMailer < ApplicationMailer
   def send_received_email(referral)
     @referral = referral
     subject = 'Thank you for contacting us'
+    subject += add_email_header(@referral)
 
-    if @referral.referral_form_type == 'medlegal'
-      subject += " Claim #: #{@referral.client_file_number}" if @referral.client_file_number.present?
 
-      subject += " File #: #{@referral.client_sub_file_number}" if @referral.client_sub_file_number.present?
-    elsif @referral.referral_form_type == 'employer'
-      subject += " Policy #: #{@referral.client_file_number}" if @referral.client_file_number.present?
-
-      subject += " Client ID #: #{@referral.client_sub_file_number}" if @referral.client_sub_file_number.present?
-    end
     mail( :to => 'peter.h.9002@gmail.com',
     :subject => subject )
     # mail( :to => @referral.rs_email,
     # :subject => 'Thank you for contacting us' )
+  end
+
+  private
+
+  def add_email_header(referral)
+    subject = ''
+
+    if referral.referral_form_type == 'medlegal'
+      subject += " Claim #: #{referral.client_file_number}" if referral.client_file_number.present?
+
+      subject += " File #: #{referral.client_sub_file_number}" if referral.client_sub_file_number.present?
+    elsif referral.referral_form_type == 'employer'
+      subject += " Policy #: #{referral.client_file_number}" if referral.client_file_number.present?
+
+      subject += " Client ID #: #{referral.client_sub_file_number}" if referral.client_sub_file_number.present?
+    end
+
+    return subject
   end
 
 end
