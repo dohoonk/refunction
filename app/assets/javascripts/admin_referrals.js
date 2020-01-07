ba2e87e944889fa74d6318e53356c5c6cbfb4ab2
Ediyu <peterhjhan@gmail.com>
2020-01-07 10:10:49 -0800

    update forms with language required on show

--- app/assets/javascripts/admin_referrals.js
+++ app/assets/javascripts/admin_referrals.js
@@ -1,11 +1,20 @@
 $( document ).on('turbolinks:load', function() {
 
   $( '.interpreter-required').on('change', 'select', function() {
-
     if ( ($(this).val() == 'Yes') ) {
       $('.interpreter-language').children().removeClass('d-none');
     } else {
       $('.interpreter-language').children().addClass('d-none');
     }
-  })
+  });
+
+
+  if( $('#showReferral').length != 0) {
+    let languageNeeded = $( '#showReferral' ).find('.interpreter-required-result div');
+    if($(languageNeeded).html().includes('Yes')) {
+      $( '#showReferral').find('.interpreter-required-language div').removeClass('d-none');
+    }
+
+  }
+
 })
