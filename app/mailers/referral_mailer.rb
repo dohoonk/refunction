9b155f3f9bab1634e0978632712581877fcff874
Dohoon Kim <dohoonk.smg@gmail.com>
2020-01-13 12:25:37 -0800

    mail addresses for refunction at the top

--- app/mailers/referral_mailer.rb
+++ app/mailers/referral_mailer.rb
@@ -1,6 +1,7 @@
 class ReferralMailer < ApplicationMailer
   default :from => 'referrals@refunction.ca'
   # send a signup email to the user, pass in the user object that   contains the user's email address
+  # info@refunction.ca,referrals@refunction.ca,referral@refunction.ca
   def send_signup_email(referral)
     @referral = referral
     mail( :to => 'info@refunction.ca,referrals@refunction.ca,referral@refunction.ca',
