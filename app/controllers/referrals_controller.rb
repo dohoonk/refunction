5dbd7df24fd13d1b35291f1b4a3063165754d74b
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2021-07-09 14:08:28 -0700

    update auto form select

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -7,10 +7,64 @@ class ReferralsController < ApplicationController
   def medlegal
     @referral = AdminReferral.new
     @admin_enquiry = AdminEnquiry.new
+
+    # Figure out the inbound link to set up selected value on the form
+    if request.referrer != nil
+      @referral_page = request.referrer.byteslice(-1).to_i
+    else 
+      @referral_page = 9
+    end
+
+    case @referral_page
+    when 9
+      @referral_page = "1 Day Functional Capacity Evaluation (FCE)"
+    when 8
+      @referral_page = "1 Day Cognitive Functional Capactiy Evaluation (Cog FCE)"
+    when 6
+      @referral_page = "Cost of Future Care Assessment"
+    when 2
+      @referral_page = "File Review/Consult"
+    else
+      @referral_page = "1 Day Functional Capacity Evaluation (FCE)"
+    end
+
   end
 
   def employer
     @referral = AdminReferral.new
     @admin_enquiry = AdminEnquiry.new
+
+     # Figure out the inbound link to set up selected value on the form
+
+     if request.referrer != nil
+      @referral_page = request.referrer.byteslice(-2..-1)
+    else 
+      @referral_page = "/9"
+    end
+  
+     case @referral_page
+     when "/5"
+      @referral_page = "Active Rehab Program"
+     when "/3"
+      @referral_page = "Brain Injury Assessment & Treatment"
+     when "/9"
+      @referral_page = "1 Day Functional Capacity Evaluation"
+     when "18"
+      @referral_page = "1 Day Cognitive Functional Capactiy Evaluation (Cog FCE)"
+     when "/1"
+      @referral_page = "Active Rehab Program"
+     when "/8"
+      @referral_page = "Ergonomic or Risk Factor Analyses"
+     when "/2"
+      @referral_page = "Brain Injury Assessment & Treatment"
+     when "11"
+      @referral_page = "Job Demands Analyses (JDA)"
+     when "14"
+      @referral_page = "Return to Work Support Services"
+     when "17"
+      @referral_page = "Psychological Assessment"
+     else
+      @referral_page = "1 Day Functional Capacity Evaluation (FCE)"
+     end
   end
 end
